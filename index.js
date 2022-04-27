bbd12f7352e0fadb605312b7df32babc48bdf91d
ankeji <ankeji1995@163.com>
2022-04-27 11:32:51 +0800

    添加判断文件是否存在的逻辑

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 const { program } = require('commander');
 const chalk = require('chalk');
-
-const createCommand = require('./command-create.js')
 const { chooseTemplate } = require('./inquirers')
 
 function start() {
@@ -13,18 +11,10 @@ function start() {
     program
         .command('create [projectName]') // <必填> [可选]
         .description('用于创建一个项目模板')
-        .option("-T, --template [template]", "输入使用的模板名字")
+        .option("-f, --force", "overwrite target directory if it exists")
         .action(async function (projectName, options) {
-            console.log(options.template);
-            let template = options.template;
             projectName = projectName || 'untitled';
-            if (!template) {
-                template = await chooseTemplate(projectName) // 注意这里是一个异步方法
-            }
-
-            console.log(chalk.rgb(69, 39, 160)('你选择的模板是  '), chalk.bgRgb(69, 39, 160)(template))
-
-            createCommand(projectName, template)
+            await chooseTemplate(projectName, options)
         })
 
     program
--- inquirers.js
+++ inquirers.js
@@ -1,6 +1,11 @@
 const inquirer = require('inquirer')
+const chalk = require('chalk');
+const fs = require('fs-extra');
+const path = require("path");
+const ora = require('ora');
+const createCommand = require('./command-create.js')
 
-async function chooseTemplate(projectName) {
+async function chooseTemplate(projectName, options) {
     const promptList = [
         {
             type: "list", // type决定交互的方式，比如当值为input的时候就是输入的形式，list就是单选，checkbox是多选...
@@ -22,10 +27,48 @@ async function chooseTemplate(projectName) {
             },
         },
     ];
-    const answers = await inquirer.prompt(promptList);  // 执行命令行交互，并将交互的结果返回
-    const { template } = answers
-    console.log(`你选择的模板是：${template}`)
-    return template  // 返回我们选择的模板
+
+    const cwd = process.cwd();
+    // 拼接到目标文件夹
+    const targetDirectory = path.join(cwd, projectName);
+    // 如果目标文件夹已存在
+    if (fs.existsSync(targetDirectory)) {
+        if (!options.force) {
+            // 如果没有设置-f则提示，并退出
+            console.error(chalk.red(`项目已经存在！请更改项目名称`))
+            return;
+        }
+        // 如果设置了-f则二次询问是否覆盖原文件夹
+        const { isOverWrite } = await inquirer.prompt([{
+            name: "isOverWrite",
+            type: "confirm",
+            message: "目标目录已存在，是否覆盖它?",
+            choices: [
+                { name: "Yes", value: true },
+                { name: "No", value: false }
+            ]
+        }]);
+        // 如需覆盖则开始执行删除原文件夹的操作
+        if (isOverWrite) {
+            const spinner = ora(chalk.blackBright('项目正在删除，请稍候...'));
+            spinner.start();
+            await fs.removeSync(targetDirectory);
+            spinner.succeed();
+            console.info(chalk.green("✨ 删除成功，启动初始化项目..."));
+            console.log();
+            // 删除成功后，开始初始化项目
+            const { template } = await inquirer.prompt(promptList);  // 执行命令行交互，并将交互的结果返回
+            console.log(chalk.rgb(69, 39, 160)('你选择的模板是  '), chalk.bgRgb(69, 39, 160)(template))
+            createCommand(projectName, template)
+        } else {
+            console.error(chalk.red(`项目已经存在！请更改项目名称去创建`))
+            return;
+        }
+    } else {
+        const { template } = await inquirer.prompt(promptList);  // 执行命令行交互，并将交互的结果返回
+        console.log(chalk.rgb(69, 39, 160)('你选择的模板是  '), chalk.bgRgb(69, 39, 160)(template))
+        createCommand(projectName, template)
+    }
 }
 
 module.exports = {
