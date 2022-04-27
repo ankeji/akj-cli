9a7583bdf5bc3cce70a37791e2176ed8dc97e7cc
ankeji <ankeji1995@163.com>
2022-04-27 15:03:12 +0800

    添加更新脚本命令

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const { program } = require('commander');
 const chalk = require('chalk');
-const { chooseTemplate } = require('./inquirers')
+const { chooseTemplate } = require('./lib/inquirers')
+const { checkUpdate } = require('./lib/checkUpdate')
 
 function start() {
     console.log(chalk.rgb(216, 27, 96)('\n      你好啊, 靓仔~~'))
@@ -13,9 +14,20 @@ function start() {
         .description('用于创建一个项目模板')
         .option("-f, --force", "overwrite target directory if it exists")
         .action(async function (projectName, options) {
-            projectName = projectName || 'untitled';
+            if (!projectName) {
+                projectName = 'project' + new Date().getTime();
+                console.log(chalk.rgb(0, 255, 127)(`由于你没有写项目名，系统以为你默认匹配名字：${projectName}`))
+            }
             await chooseTemplate(projectName, options)
         })
+
+    program.command("update")
+        .description("将cli更新至最新版本")
+        // update命令执行后做的事情，自动检测更新
+        .action(async () => {
+            await checkUpdate();
+            console.log('update');
+        });
 
     program
         .command('help')
